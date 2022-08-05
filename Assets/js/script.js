538c1fef7d4eedc5cb40ef4668963c2275f36d44
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-08-05 15:16:17 -0700

    updated website working 100%

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,7 +13,12 @@ function displayTime() {
 }
 displayTime ();
 
+function saveData() {
+   var textareaData = $(this).siblings(".description").val();
+   var key = $(this).parent().attr("id");
+   localStorage.setItem(key, textareaData);
 
+}
 
 
 console.log(moment().format());
@@ -40,6 +45,37 @@ function checkHour(){
 })
 }
 checkHour();
+
+function previousData(){
+    $("#hour-9").val(localStorage.getItem("9"))
+    $("#hour-10").val(localStorage.getItem("10"))
+    $("#hour-11").val(localStorage.getItem("11"))
+    $("#hour-12").val(localStorage.getItem("12"))
+    $("#hour-13").val(localStorage.getItem("13"))
+    $("#hour-14").val(localStorage.getItem("14"))
+    $("#hour-15").val(localStorage.getItem("15"))
+    $("#hour-16").val(localStorage.getItem("16"))
+    $("#hour-17").val(localStorage.getItem("17"))
+
+}
+
+previousData();
+
+save.on("click", saveData)
+
+
+
+//When the page loads, any data in local storage for each time block should show in the corresponding text area.
+//
+
+
+
+
+
+
+
+
+
 // var currentHour = moment().format('h');
 // var nine=$('#9');
 // if(hour === currentHour){
